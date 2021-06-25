86009ab363b8dafecd6f7761659cc296bd00a37f
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-06-25 16:26:09 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -22,8 +22,8 @@ class Author
     Post.all.select {|post| post.author == self} 
   end
   
-  def add_post_by_title(post)
-    post = Post.new(name)
+  def add_post_by_title(title)
+    post = Post.new(title)
     @@all << post
     post.author = self
     @@post_count += 1
